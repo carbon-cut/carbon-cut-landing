73cc951830e411b54cce5166a26a84f0da45e82b
Karim-Amouri <103941991+Karim-Amouri@users.noreply.github.com>
2025-01-11 01:33:00 +0100

    assets fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  assetPrefix: './',
+  basePath: "/carbon-cut-landing",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
